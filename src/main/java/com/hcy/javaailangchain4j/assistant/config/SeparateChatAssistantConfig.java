3a8ab408082cf064a750887de5698a7d1dbb1442
hcy <1120437670@qq.com>
2025-05-20 22:08:35 +0800

    feat: 替换聊天记忆的存储类

--- src/main/java/com/hcy/javaailangchain4j/assistant/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/hcy/javaailangchain4j/assistant/config/SeparateChatAssistantConfig.java
@@ -2,6 +2,7 @@ package com.hcy.javaailangchain4j.assistant.config;
 
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,15 @@ public class SeparateChatAssistantConfig {
     @Bean
     public ChatMemoryProvider chatMemoryProvider() {
         //此处maxMessages就是最大记忆轮次，因此在eparateChatAssistant类中的@AiService将不需要再配置chatMemory属性
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+        return memoryId -> MessageWindowChatMemory.builder()
+                .id(memoryId)
+                .maxMessages(10)
+                //聊天记忆的存储默认使用的是 dev.langchain4j.memory.chat.SingleSlotChatMemoryStore
+                //SingleSlotChatMemoryStore和InMemoryChatMemoryStore的实现方式不一样
+                //SingleSlotChatMemoryStore：memoryId+messages
+                //而InMemoryChatMemoryStore是用的ConcurrentHashMap，memoryId为key,messages为value
+                //InMemoryChatMemoryStore更适合多线程
+                .chatMemoryStore(new InMemoryChatMemoryStore())
+                .build();
     }
 }
